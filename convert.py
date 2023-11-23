d6b1e4c71c8ab0382d229c893faef7871061e18c
Mubashir Tanwar <tanwar0210@gmail.com>
2023-11-24 02:46:38 +0530

    Initial Commit

---
+++ convert.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+import pytesseract
+from PIL import Image
+import sys
+
+if len(sys.argv) != 3:
+    sys.exit("Usage: python convert.py filename outputFilename")
+
+image_file = sys.argv[1]
+output = sys.argv[2] + ".txt"
+out_directory = Path(r"~\Desktop").expanduser()
+text_file = out_directory / Path(output)
+
+with open(text_file, "a", encoding="utf-8") as output_file:
+    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"  # only use this is pytesseract is not stored in path
+    text = str(pytesseract.image_to_string(Image.open(image_file)))
+    text = text.replace("-\n", "")
+    output_file.write(text)
